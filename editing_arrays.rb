6812b218e4b8a238a3dc9e95ada21276daca448d
GeekyD1no <21achahn@bishopkelley.org>
2018-07-11 14:26:17 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,10 +4,10 @@ rainbow_colors = [
   "light_cyan"
 ]
 
-def change_rainbow_colors
-  "red"
+def change_rainbow_colors =
+  ["red"
   "light_red"
-  "light_yellow"
+  "light_yellow"] 
 end
 
 def add_colors
